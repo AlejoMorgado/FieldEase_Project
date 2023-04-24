a3215a1edfa036f54b8c32b1dfb873d8e22e158e
Alejandro Morgado <alejo.morgado2@gmail.com>
2023-04-23 23:12:22 -0500

    Test the endpoint

--- src/index.js
+++ src/index.js
@@ -8,20 +8,6 @@ app.use(express.json())
 
 app.use("/api/sensorsData", v1Router);
 
-
-
-// app.get("/sensorData", (req,res) => {
-//     res.send('<h1> Holita</h1>');
-// });
-// app.post("/sensorData", (req,res) => {
-//     console.log(req.body);
-//     res.send(req.body);
-// });
-// app.get("/", (req,res) => {
-//     res.send('<h1> Hello World </h1>');
-// });
-
-
 app.listen(PORT, ()=>{
     console.log(`server listening on port ${PORT}`)
 });
