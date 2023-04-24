9d6f4c9c9a3bd85a315b5bc088792cafe67478fe
Alejandro Morgado <alejo.morgado2@gmail.com>
2023-04-23 23:40:52 -0500

    change the localization of the map

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 4.5504627, lng: -75.6564232},
-    zoom: 12,
+    center: { lat: 4.570635, lng: -75.683631},
+    zoom: 18,
   });
 }
 
