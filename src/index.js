2cc2767fd58ec6f49e03907415153fa1f175d3d4
SantiagoMurilloV <128425308+SantiagoMurilloV@users.noreply.github.com>
2023-04-25 18:58:53 -0500

    delete the old api and restructure the api. Restructere the api so we can post the inforrmation to the data base, we connect the api with the data base

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
-const express  = require('express');
-const v1SensorsRouter = require("./V1/routes/sensorRoutes");
-
+const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'santi',
+    database: 'fieldease'
+});
+
 app.use(express.json());
 
-app.use("/api/sensorsData", v1SensorsRouter);
+app.post('/api/sensorsData', (req, res) => {
+    const { currrent_date, temperature, humidity } = req.body;
 
-app.use(express.json())
+
+    if (!currrent_date || !temperature || !humidity) {
+        res.status(400).send('Los datos recibidos no son válidos');
+        return;
+    }
 
 
-app.listen(PORT, ()=>{
-    console.log(`server listening on port ${PORT}`)
+    const sql = 'INSERT INTO users  VALUES (?, ?, ?)';
+    connection.query(sql, [currrent_date, temperature, humidity], (error, results, fields) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send(error);
+            return;
+        }
+        console.log(`Datos insertados correctamente: ${currrent_date}, ${temperature}, ${humidity}`);
+        res.sendStatus(200);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
