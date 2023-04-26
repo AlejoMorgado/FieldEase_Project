8842608e8543d24165068281f6ca8fd30ab70cc0
Alejandro Morgado <alejo.morgado2@gmail.com>
2023-04-26 13:58:51 -0500

    Restructure the index.js file and changed names of variables. Made eventlisteners to display the information that i want. Also add a little bit of css style.

--- js/index.js
+++ js/index.js
@@ -1,102 +1,78 @@
 let map;
 let infoWindow;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 18,
-    center: { lat: 4.569983, lng: -75.684261 },
-    mapTypeId: "satellite",
-    disableDefaultUI: true
-  });
+// Initialize the map
+const initMap = () => {
+// Set map options
+map = new google.maps.Map(document.getElementById("map"), {
+zoom: 18,
+center: { lat: 4.569983, lng: -75.684261 },
+mapTypeId: "satellite",
+disableDefaultUI: true
+});
 
-  // Define the LatLng coordinates for the polygon.
-  const triangleCoords = [
-    { lat: 4.569785, lng: -75.683658 },
-    { lat: 4.569293, lng: -75.683755 },
-    { lat: 4.568619, lng: -75.684334 },
-    { lat: 4.569175, lng: -75.685621 },
-    { lat: 4.570101, lng: -75.685227 },
-  ];
-  // Construct the polygon.
-  const bermudaTriangle = new google.maps.Polygon({
-    paths: triangleCoords,
-    strokeColor: "#FF0000",
-    strokeOpacity: 0.8,
-    strokeWeight: 3,
-    fillColor: "#FF0000",
-    fillOpacity: 0.35,
-  });
-  const lineCoords = [
-    { lat: 4.570887, lng: -75.683749 },
-    { lat: 4.569785, lng: -75.683658 },
-    { lat: 4.570101, lng: -75.685227 },
-    { lat: 4.570878, lng: -75.684918 },
+const redPolygonCoords = [
+{ lat: 4.569785, lng: -75.683658 },
+{ lat: 4.569293, lng: -75.683755 },
+{ lat: 4.568619, lng: -75.684334 },
+{ lat: 4.569175, lng: -75.685621 },
+{ lat: 4.570101, lng: -75.685227 },
+];
 
-  ];
-  // Construct the polygon.
-  const segment = new google.maps.Polygon({
-    paths: lineCoords,
-    strokeColor: "blue",
-    strokeOpacity: 0.8,
-    strokeWeight: 5,
-    fillColor: "blue",
-    fillOpacity: 0.35,
-  });
-  segment.setMap(map);
-  bermudaTriangle.setMap(map);
-  segment.addListener("click", showArrays2);
-  infoWindow = new google.maps.InfoWindow();
-  bermudaTriangle.addListener("click", showArrays);
-  infoWindow = new google.maps.InfoWindow();
+const bluePolygonCoords = [
+{ lat: 4.570887, lng: -75.683749 },
+{ lat: 4.569785, lng: -75.683658 },
+{ lat: 4.570101, lng: -75.685227 },
+{ lat: 4.570878, lng: -75.684918 },
+];
+
+const redPolygon = new google.maps.Polygon({
+paths: redPolygonCoords,
+strokeColor: "#FF0000",
+strokeOpacity: 0.8,
+strokeWeight: 3,
+fillColor: "#FF0000",
+fillOpacity: 0.35,
+});
+
+const bluePolygon = new google.maps.Polygon({
+paths: bluePolygonCoords,
+strokeColor: "blue",
+strokeOpacity: 0.8,
+strokeWeight: 5,
+fillColor: "blue",
+fillOpacity: 0.35,
+});
+
+redPolygon.setMap(map);
+bluePolygon.setMap(map);
+
+redPolygon.addListener("click", showRedPolygonInfo);
+bluePolygon.addListener("click", showBluePolygonInfo);
+
+infoWindow = new google.maps.InfoWindow();
 }
-const infoDiv = document.createElement("div");
-infoDiv.id = 'redbermuda'
-document.body.appendChild(infoDiv);
-const infoDiv2 = document.createElement("div");
-infoDiv2.id = 'bluesegment'
-document.body.appendChild(infoDiv2);
 
-function showArrays(event) {
-  const polygon = this;
-  const vertices = polygon.getPath();
-  let contentString =
-    "<b>Bermuda Triangle polygon</b><br>" +
-    "Clicked location: <br>" +
-    event.latLng.lat() +
-    "," +
-    event.latLng.lng() +
-    "<br>";
+const redPolygonInfoDiv = document.createElement("div");
+redPolygonInfoDiv.id = 'red-polygon-info';
+graphicsContainer.appendChild(redPolygonInfoDiv);
 
-  for (let i = 0; i < vertices.getLength(); i++) {
-    const xy = vertices.getAt(i);
+const bluePolygonInfoDiv = document.createElement("div");
+bluePolygonInfoDiv.id = 'blue-polygon-info';
+graphicsContainer.appendChild(bluePolygonInfoDiv);
 
-    contentString +=
-      "<br>" + "Coordinate " + i + ":<br>" + xy.lat() + "," + xy.lng();
-  }
-
-  // Update the content of the info div.
-  infoDiv.innerHTML = contentString;
+let paragraphRedPolygon = document.createElement("p");
+const showRedPolygonInfo = () => {
+  let contentString = "Sector 1 data";
+  paragraphRedPolygon.textContent = contentString;
+  redPolygonInfoDiv.appendChild(paragraphRedPolygon);
 }
-// You can add this code to your existing code and modify the initMap function to use the showArrays function as the event listener for the polygon's click event.
 
-function showArrays2(event) {
-  const polygon = this;
-  const vertices = polygon.getPath();
-  let contentString =
-    "<b>Segment Triangle polygon</b><br>" +
-    "Clicked location: <br>" +
-    event.latLng.lat() +
-    "," +
-    event.latLng.lng() +
-    "<br>";
-
-  for (let i = 0; i < vertices.getLength(); i++) {
-    const xy = vertices.getAt(i);
-
-    contentString +=
-      "<br>" + "Coordinate " + i + ":<br>" + xy.lat() + "," + xy.lng();
-  }
-  // Update the content of the info div.
-  infoDiv2.innerHTML = contentString;
+let paragraphBluePolygon = document.createElement("p");
+const showBluePolygonInfo = () => {
+  let contentString = "Sector 2 data";
+  paragraphBluePolygon.textContent = contentString;
+bluePolygonInfoDiv.appendChild(paragraphBluePolygon)
 }
+
 window.initMap = initMap;
